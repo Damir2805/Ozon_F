2226ceb667655f359d2c27b4592d3284298d239c
Дамир Калымбаев <d_kalymbaev@mail.ru>
2020-05-29 09:29:00 +0300

    Initial commit

---
+++ F.rb
@@ -0,0 +1,21 @@
+﻿current_path = File.dirname(__FILE__)
+
+file_path = current_path + "/output.txt"
+
+target = STDIN.gets.chomp.to_i
+
+numsline = STDIN.gets.split().map(&:to_i)
+
+diff = numsline.map { |e| target - e }
+
+nums = diff & numsline
+
+if nums.length >= 2
+	file = File.new(file_path, "a:UTF-8")
+	file.print("1\n\r")
+	file.close
+else
+	file = File.new(file_path, "a:UTF-8")
+	file.print("0\n\r")
+	file.close
+end
